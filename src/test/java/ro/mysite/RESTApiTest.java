9a200bbdd2dd4f29dd956e2429bf46c302002419
CrisAce <cristi.ion21@gmail.com>
2019-06-03 17:30:56 +0300

    second commit

--- src/test/java/ro/mysite/RESTApiTest.java
+++ src/test/java/ro/mysite/RESTApiTest.java
@@ -8,6 +8,8 @@ public class RESTApiTest {
 	@Test
 	public void postJira() {
 		System.out.println("post Jira");
+		System.out.println("post Jira 2");
+		System.out.println("post Jira 3");
 	}
 
 	@Test
